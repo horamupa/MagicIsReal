a95b3d75555a275cb67f75aa7ffb4102049bfaa6
horamupa <horamupa@gmail.com>
2023-06-07 14:05:32 +0300

    Initial Commit

---
+++ MagicIsReal/MagicIsRealApp.swift
@@ -0,0 +1,17 @@
+//
+//  MagicIsRealApp.swift
+//  MagicIsReal
+//
+//  Created by MM on 07.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct MagicIsRealApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
